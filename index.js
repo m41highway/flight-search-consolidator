31fa8ca9ba152f1fb0760b29e035be4cd934d63c
Anthony Ng <anthony.ng.ags@gmail.com>
2017-07-12 17:39:18 +0800

    Add the consolidator basic structure

---
+++ index.js
@@ -0,0 +1,54 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const fetch = require('node-fetch');
+
+const mistifly = require('./flights/search-mystifly');
+
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+// app.use(logger());
+
+app.use(router.routes());
+
+app.listen(8200);
+
+router.get('/', async function (ctx){
+    ctx.body = 'Hello from Flight Seach Consolidator';
+});
+
+
+// ----------------------------------------------------
+// Search Flight API
+// Usage:
+// POST http://localhost:8200/flights/search
+// {
+// 	  "origin": "DEL",
+// 	  "destination": "SIN",
+// 	  "departDate": "2017-08-15",
+// 	  "returnDate": "2017-08-19"
+// }
+// ----------------------------------------------------
+router.post('/flights/search', async function (ctx){
+
+    const departDate = ctx.request.body.departDate;
+    const returnDate = ctx.request.body.returnDate;
+    const origin = ctx.request.body.origin;
+    const destination = ctx.request.body.destination;
+    const cabinType = ctx.request.body.cabinType;
+
+    // let result = await mystiflySearch(departDate, returnDate, origin, destination, cabinType);
+
+    // let result = await Promise.all([ mystiflySearch(departDate, returnDate, origin, destination, cabinType) ])
+
+    let result = await Promise.all([ mistifly.search(departDate, returnDate, origin, destination, cabinType) ])
+
+
+    ctx.body = result;
+});
+
